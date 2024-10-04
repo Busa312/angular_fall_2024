1fa294ffea43d234184d498d4e1123a35237dfb1
busa312 <besarionigadelia@gmail.com>
2024-10-04 14:22:54 +0400

    lecture_2

---
+++ Lecture_2/source/index.js
@@ -0,0 +1,176 @@
+let age = 16;
+
+// boolean type: 
+// true, false
+
+
+age = 20;
+
+// console.log(age < 18);
+// if (age < 18) {
+//     console.log('restricted');
+// }
+
+// if (age < 18) {
+//     console.log('restricted');
+// } else {
+//     console.log('you can enter');
+// }
+
+// age = 25;
+
+// if (age < 18) {
+//     console.log('school');
+// } else if (age < 24) {
+//     console.log('university');
+// } else {
+//     console.log('work');
+// }
+
+// let day = 'asjhdbjahsbdjhqwbdj';
+
+// equality - == === 
+// not equal - != !==
+
+// if (day === 0) {
+//     console.log('Monday');
+// } else if (day === 1) {
+//     console.log('Tuesday');
+// } else if (day === 2) {
+//     console.log('Wednesday');
+// } else {
+//     console.log('weekend');
+// }
+
+age = 17;
+
+// let message = age > 18 ? 'can enter' : 'restricted';
+// // if condition is true: 'can enter';
+// // if condition is false: 'restricted';
+
+// console.log(message);
+let message;
+
+// if (age > 18) {
+//     message = 'can enter'
+// } else {
+//     message = 'restricted';
+// }
+
+// console.log(message);
+
+// age = 17;
+
+// message = age < 18 ? 'hi' : 
+//     (age < 21) ? 'wow' :
+//     (age < 30) ? 'Hello' : 
+//     (age < 50) ? 'yayyy' : 'welcome';
+
+// console.log(message);
+
+
+// if (age < 18) {
+//     console.log('hi');
+// } else if (age < 21) {
+//     console.log('wow');
+// } else if (age < 30) {
+//     console.log('Hello');
+// } else if (age < 50) {
+//     console.log('yayyy');
+// } else {
+//     console.log('welcome');
+// }
+
+
+// let dayNumber = 2;
+
+// switch (dayNumber) {
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         console.log('yaay')
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     default:
+//         console.log('weekend');
+// }
+
+// logical operators: 
+// AND - &&
+// OR - ||
+// NOT - !
+// EQUAL - ===
+// NOT EQUAL - !==
+
+// age = 19;
+
+// let bool = !(age > 18) && age < 21;
+
+// bool = !(age > 18 || age < 10);
+
+// console.log(bool)
+
+
+// let not = !!false;
+
+// console.log(not);
+
+// bool = age < 5 && age > 20;
+
+// let ragacSia; // undefined;
+
+// if (!ragacSia) {
+//  console.log('rame errori');
+// }
+
+// falsy: 0, '', null, undefined;
+
+
+// let i = 0;
+
+// while (i < 3) {
+//     console.log(i);
+//     // i = i + 1 === i++ 
+//     i++;
+// }
+
+// do {
+//     console.log(i);
+//     i++;
+//     // i+=1
+// } while (i < 3 && i > 0);
+
+// console.log(1)
+// console.log(2)...
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
+
+// let j;
+
+// for (j = 20; j > 0; j--) {
+//     console.log(j)
+// }
+
+// for (let x = 1; x < 20; x*=2) {
+//     console.log(x);
+// }
+
+// + - / * %
+
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log('EVEN');
+//     } else {
+//         console.log('ODD');
+//     }
+// }
+
+// for (let i = 10; i <= 20; i++) {
+//     console.log(i);
+// }
